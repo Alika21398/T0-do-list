f41a8eb81150969a9f0144e5659db45e33dcb27d
Alika <dangolalika2017@gmail.com>
2024-09-30 00:09:31 +0545

    improved CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Bg from "./bgg.jpg";
+import "./App.js";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -48,18 +49,18 @@ function App() {
       <div className=" relative min-h-screen object-cover ">
         <img className="relative w-full h-screen" src={Bg} alt="" />
 
-        <div className="lg:h-[700px] lg:w-[670px]  bg-[#BE3E4D] m-auto rounded-xl shadow-xl absolute left-[30%] top-5 overflow-auto">
-          <div className="text-center text-3xl font-bold text-[#ffffff] p-5">
+        <div className="lg:h-[700px] lg:w-[670px]  bg-[#BE3E4D]/20 backdrop-blur-lg m-auto rounded-xl shadow-xl absolute left-[30%] top-5 overflow-y-auto">
+          <div className="text-center text-3xl font-bold text-[#8f2d5b] p-5">
             <h1>To-Do-List</h1>
           </div>
 
-          <div className="border-[#d56e2d] border-2 m-8 text-white ">
+          <div className="border-[#d56e2d] border-2 m-8 text-white rounded-lg ">
             <form
-              className="m-4 flex justify-between gap-5 text-black"
+              className="m-4 flex justify-between gap-5 "
               onSubmit={addTask}
             >
               <input
-                className="bg-[#BE3E4D] text-white flex-grow pl-5 border"
+                className=" bg-[#BE3E4D]/20 backdrop-blur-lg text-black flex-grow pl-5 border placeholder-[#521c35]"
                 placeholder="Enter Your Task"
                 value={inputTask}
                 onChange={(event) => setInputTask(event.target.value)}
@@ -67,43 +68,45 @@ function App() {
               <button
                 type="submit"
                 disabled={inputTask.length == 0}
-                className="px-4 py-2 bg-[#D4B41B] rounded-lg"
+                className="px-4 py-2 bg-[#8f2d5b] rounded-lg"
               >
                 Add
               </button>
               <div></div>
             </form>
-            <ol className="m-4 ">
+            <div className="m-4 ">
               {tasks.map((item, index) => {
                 return (
-                  <li
-                    className="cursor-pointer flex justify-between gap-5 my-4 "
+                  <div
+                    className="cursor-pointer flex justify-between items-center gap-5 my-4 flex-wrap "
                     key={index}
                   >
                     {" "}
-                    <p
-                      className={`flex-grow font-light text-2xl ${
+                    <div
+                      className={`flex flex-grow flex-wrap p-3 text-black font-normal text-2xl inconsolata-font w-[200px] border rounded-lg  ${
                         item.isCompleted ? "line-through" : ""
                       }`}
                     >
                       {item.task}
-                    </p>
-                    <button
-                      onClick={() => onClickTask(index)}
-                      className={`bg-[#D7B91B] px-4 py-2 `}
-                    >
-                      {item.isCompleted ? "Undo" : "Done"}
-                    </button>
-                    <button
-                      onClick={() => deleteTask(index)}
-                      className="bg-red-600 px-4 py-2"
-                    >
-                      Delete
-                    </button>
-                  </li>
+                    </div>
+                    <div className="flex gap-5 ">
+                      <button
+                        onClick={() => onClickTask(index)}
+                        className={`bg-[#8f2d5b] h-fit px-4 py-3 rounded-lg `}
+                      >
+                        {item.isCompleted ? "Undo" : "Done"}
+                      </button>
+                      <button
+                        onClick={() => deleteTask(index)}
+                        className="bg-red-600 px-4 py-3 h-fit rounded-lg"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
                 );
               })}
-            </ol>
+            </div>
           </div>
         </div>
       </div>
